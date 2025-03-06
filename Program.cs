88e03956a5e99844e96f69a6695a35df15fca0f2
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-03-06 18:22:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escreva um valor inteiro entre 1 e 1000:");
+        if (int.TryParse(Console.ReadLine(), out int X) && X >= 1 && X <= 1000)
+        {
+            Console.WriteLine($"Numeros impares de 1 ate {X}:");
+            for (int i = 1; i <= X; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        else
+        {
+        }
+    }
+}
